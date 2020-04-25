462312d0ff90c7a5cb438eba9a54c301c5404d58
¨Gober¨ <reyesfragosoroberto@gmail.com>
2020-04-25 16:03:09 -0500

    [Changed] Validated that the path always exist

--- src/input.rs
+++ src/input.rs
@@ -23,34 +23,3 @@ pub struct Command {
     #[structopt(long)]
     pub secondary_color: Option<String>,
 }
-/*
-pub fn get_cli_arguments(user_input: Command) {
-    // You should validate the path if there is a path
-    // You should validate first and secondary color
-    let mut u_word = String::new();
-    let mut u_path = PathBuf::new();
-    let mut u_p_color = String::new();
-    let mut u_s_color = String::new();
-
-    match user_input.word_to_search {
-        Some(word_to_search) => u_word = word_to_search,
-        None => (),
-    }
-
-    match user_input.path {
-        Some(path) => u_path = path,
-        None => (),
-    }
-
-    match user_input.primary_color {
-        Some(primary_color) => u_p_color = primary_color,
-        None => println!(),
-    }
-
-    match user_input.secondary_color {
-        Some(secondary_color) => u_s_color = secondary_color,
-        None => println!(),
-    }
-
-    return [u_word, path, u_p_color, u_s_color];
-}*/
--- src/settings.rs
+++ src/settings.rs
@@ -1,80 +1,134 @@
+/* TODO
+    - Check that the file exists
+    - Check that the color option matches
+*/
+
 use crate::input;
 use ansi_term::Colour;
-use ansi_term::{ANSIString, ANSIStrings};
-use std::collections::HashMap;
 use std::path::PathBuf;
 
 extern crate preferences;
-use preferences::{AppInfo, Preferences, PreferencesMap};
+use preferences::{prefs_base_dir, AppInfo, Preferences, PreferencesMap};
 
 const APP_INFO: AppInfo = AppInfo {
     name: "preferences",
     author: "mme",
 };
-const KEY: &str = "config";
+const KEY: &str = "mme";
 
 pub struct Config {
-    pub commands_path: String, //Change to StringPath
+    pub commands_path: PathBuf,
     pub title_color: ansi_term::Colour,
     pub information_color: ansi_term::Colour,
 }
 
 impl Config {
     pub fn new(user_input: input::Command) -> Result<Config, &'static str> {
-        let mut new_path = PathBuf::new();
-        let mut new_primary_color = String::new();
-        let mut new_secondary_color = String::new();
+        let mut current_path = PathBuf::new();
+        let mut current_primary_color = String::new();
+        let mut current_secondary_color = String::new();
+        let mut new_config = false;
 
         match user_input.path {
-            Some(path) => new_path = path,
+            Some(path) => current_path = path,
             None => (),
         }
 
         match user_input.primary_color {
-            Some(primary_color) => new_primary_color = primary_color,
+            Some(primary_color) => current_primary_color = primary_color,
             None => (),
         }
 
         match user_input.secondary_color {
-            Some(secondary_color) => new_secondary_color = secondary_color,
+            Some(secondary_color) => current_secondary_color = secondary_color,
             None => (),
         }
 
-        if !new_path.as_os_str().is_empty()
-            || !new_primary_color.is_empty()
-            || !new_secondary_color.is_empty()
+        if !current_path.as_os_str().is_empty()
+            || !current_primary_color.is_empty()
+            || !current_secondary_color.is_empty()
         {
-            change_configuration(new_path, new_primary_color, new_secondary_color)
+            new_config = true;
         }
 
+        let mut user_preferences = match PreferencesMap::<String>::load(&APP_INFO, &KEY) {
+            // Try to find previous preferences
+            Ok(user_preferences) => {
+                // If it finds a preference, check that the path is in it
+                if user_preferences.contains_key(&String::from("commands_path")) {
+                    user_preferences
+                } else {
+                    // If it doesn't find the path but has the color configurations,
+                    // check that the user provided a new path
+                    if current_path.as_os_str().is_empty() {
+                        return Err("Path not configured yet inside");
+                    } else {
+                        user_preferences
+                    }
+                }
+            }
+            Err(_) => {
+                // If the preferences doesn't exist and the user didn't type the new path
+                if current_path.as_os_str().is_empty() {
+                    return Err("Path not configured yet");
+                } else {
+                    PreferencesMap::new()
+                }
+            }
+        };
+
+        let mut path_match = &String::from("commands_path");
+        let mut fc_match = &String::from("head_color");
+        let mut sc_match = &String::from("text_color");
+
+        if new_config {
+            user_preferences.insert(
+                path_match.to_string(),
+                current_path.clone().into_os_string().into_string().unwrap(),
+            );
+            user_preferences.insert(fc_match.to_string(), current_primary_color);
+            user_preferences.insert(sc_match.to_string(), current_secondary_color);
+            match user_preferences.save(&APP_INFO, &KEY) {
+                Ok(_) => println!("Configuration saved inside: {:?}", prefs_base_dir()),
+                Err(_) => return Err("Cannot save your preferences"),
+            }
+        }
+
+        /*
+            // Assign the new values to the configuration
+            for (key, val) in user_preferences.iter() {
+                match (key, val) {
+                    (path_match, val) if current_path.as_os_str().is_empty() => {
+                        current_path = PathBuf::from(val)
+                    }
+                    (fc_match, val) if current_primary_color.is_empty() => {
+                        current_primary_color = String::from(val)
+                    }
+                    (sc_match, val) if current_secondary_color.is_empty() => {
+                        current_secondary_color = String::from(val)
+                    }
+                    _ => (),
+                }
+            }
+        */
+
         Ok(Config {
-            commands_path: String::from("aaa"),
+            commands_path: current_path,
             title_color: Colour::White,
             information_color: Colour::White,
         })
     }
-}
 
-fn change_configuration(path: PathBuf, primary_color: String, secondary_color: String) {
-    let mut user_configurations = read_to_change_configuration();
-    user_configurations.insert(
-        String::from("command_path"),
-        path.as_path().display().to_string(), //Note, we should check the path before assign it here
-    );
-    println!("{:?}", user_configurations);
-    update_configuration(user_configurations);
-}
-
-fn read_to_change_configuration() -> PreferencesMap {
-    match PreferencesMap::<String>::load(&APP_INFO, &KEY) {
-        Ok(preferences) => return preferences,
-        Err(_) => return PreferencesMap::new(),
-    }
-}
-
-fn update_configuration(updated_configuration: PreferencesMap) {
-    match updated_configuration.save(&APP_INFO, &KEY) {
-        Ok(_) => (),
-        Err(e) => println!("{}", e),
+    fn get_color(str_color: String) -> Colour {
+        match str_color.to_lowercase().as_str() {
+            "black" => Colour::Cyan,
+            "red" => Colour::Red,
+            "green" => Colour::Green,
+            "yellow" => Colour::Yellow,
+            "blue" => Colour::Blue,
+            "purple" => Colour::Purple,
+            "cyan" => Colour::Cyan,
+            _ => Colour::White,
+        }
     }
 }
