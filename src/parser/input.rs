00fe3839e30d01d66b9f5009f326cd0767584d6a
¨Gober¨ <reyesfragosoroberto@gmail.com>
2020-08-06 21:17:41 -0500

    Document the input parser

--- src/parser/input.rs
+++ src/parser/input.rs
@@ -13,15 +13,19 @@ pub struct Command {
     #[structopt(short = "d", long = "desc")]
     pub search_only_in_desc: bool,
 
+    /// Sets the path of the file where mme is going to search
     #[structopt(short = "p", long)]
     pub path: Option<String>,
 
+    ///Sets the color of NAME, DESC words
     #[structopt(short = "f", long)]
     pub fixed_color: Option<String>,
 
+    /// Sets the color of the text
     #[structopt(short = "t", long)]
     pub text_color: Option<String>,
 
+    ///Sets the color of the highlighted word
     #[structopt(short = "h", long)]
     pub highlight_color: Option<String>,
 }
